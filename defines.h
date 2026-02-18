fff4161fbabf43970b961ec2fe98521bfd25f5f3
novacoviciionut000-ux <nova@DESKTOP-4CNJOE7.localdomainnovacoviciionut000@gmail.com>
2026-02-18 03:08:53 +0200

    Nothing significant

--- defines.h
+++ defines.h
@@ -3,9 +3,7 @@
 #define SCREEN_WIDTH 1024
 #define SCREEN_HEIGHT 768
 #define FOCAL 100 // this is the focal length, it is used for perspective projection
-// defines.h
-// Toggle this to switch the entire engine
-// #define USE_DOUBLE_PRECISION 
+#define USE_DOUBLE_PRECISION 
     #ifdef USE_DOUBLE_PRECISION
         typedef double real;
     #else
